a4ad2a0172363966a569d5cb2a57bda7676e8b83
eesha <79874478+beepboopblorp@users.noreply.github.com>
2025-12-25 14:33:07 -0500

    Add files via upload

--- api/topics.js
+++ api/topics.js
@@ -25,7 +25,8 @@ export default async function handler(req, res) {
                 temp = data.substring(0, data.indexOf('\n'));
             }
             do {
-                let question = answer = "";
+                let question = "";
+                let answer = "";
                 if (temp.substring(0, temp.indexOf('\t')) != "") {
                     question = temp.substring(0, temp.indexOf('\t'));
                     temp = temp.substring(temp.indexOf('\t') + 1);
